a7f9c92195e6c99dd80b3646ce494ceda565d735
“jhirsch1” <jacob_hirschhorn@brown.edu>
2024-04-19 15:12:50 -0400

    preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -30,4 +30,52 @@ def preprocess():
         new_fp = "data/cars-br-cropped/img_" + zeroes + str(i) + ".jpg"
         im.save(new_fp)
 
-preprocess()
+# preprocess()
+
+def get_labels():
+    plates = []
+    for i in range(1,5001):
+        num_zeroes = 6 - len(str(i))
+        zeroes = ""
+        for j in range(num_zeroes):
+            zeroes += "0" # file name purposes
+        txt_fp = "data/cars-br/img_" + zeroes + str(i) + ".txt"
+        # open txt file with car information
+        with open(txt_fp, "r") as txt_file:
+            file_contents = txt_file.readlines()
+            plate_id = file_contents[1].split(" ")
+            # get the plate values from the txt files
+            plate = plate_id[1]
+            # add plate values to labels array
+            plates.append(plate)
+    return np.array(plates)
+# plates = get_labels()
+# print(plates[0])
+# print(plates[10])
+# print(plates[-1])
+# print(len(plates))
+
+def get_inputs():
+    ## 24 is the width, 94 is the height of the image
+    # We chose these dimensions to be consistent with the basis paper
+    images = np.zeros(shape=(5000, 24, 94, 3))
+    for i in range(1,5001):
+        num_zeroes = 6 - len(str(i))
+        zeroes = ""
+        for j in range(num_zeroes):
+            zeroes += "0" # file name purposes
+        img_fp = "data/cars-br-cropped/img_" + zeroes + str(i) + ".jpg"
+        # open image and convert to RGB values
+        im = Image.open(img_fp)
+        im = im.convert('RGB')
+        # Resize to be consistent with paper
+        im = im.resize((94,24))
+        im_array = np.array(im, dtype='float64')
+        # normalize pixel values
+        im_array /= 255.0
+        # add to input array
+        images[i-1] = im_array
+    return images
+
+images = get_inputs()
+print(images.shape)

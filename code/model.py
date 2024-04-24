82881ee6fef4f538b947e4f6ffcd8525b5bf2b56
“jhirsch1” <jacob_hirschhorn@brown.edu>
2024-04-24 10:46:03 -0400

    done basic block

--- code/model.py
+++ code/model.py
@@ -4,9 +4,16 @@ import tensorflow as tf
 
 class small_basic_block(tf.keras.Module):
     def __init__(self, channel_in, channel_out):
-        pass
+        self.basic_block = tf.keras.Sequential(
+            layers = [
+                tf.keras.layers.Conv2D(filters = (channel_out // 4), kernel_size = 1, activation="relu"),
+                # padding is more basic in tf, so this is not an exact replica of the padding used in the paper
+                tf.keras.layers.Conv2D(filters = (channel_out // 4), kernel_size = (3,1), padding="same", activation="relu"),
+                tf.keras.layers.Conv2D(filters = (channel_out // 4), kernel_size = (1,3), padding="same", activation="relu"),
+                tf.keras.layers.Conv2D(filters = channel_out, kernel_size=1)
+            ])
     def forward(self, x):
-        pass
+        return self.basic_block(x)
 
 
 class LPRNetModel(tf.keras.Model):
